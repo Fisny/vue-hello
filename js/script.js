5a4708f90f27caf197d54073254a23d64e4931a8
Fisny <bardi.fisniku@gmail.com>
2021-06-25 09:27:46 +0200

    es

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Descrizione: Stampare a schermo un messaggio 
+// all’interno di un h1, utilizzando i data.
+
+const app = new Vue(
+    {
+        el: "#app",
+        data: {
+            txt: "ciao come stai?"
+        }
+    }
+)
